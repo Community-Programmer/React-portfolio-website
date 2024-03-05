4d3d878e990a62dd67a4f9d82f47f47275c65ad0
Sarthak Patel <sarthakpatel230204@gmail.com>
2024-03-05 23:00:26 +0530

    configured dashboard page

--- src/Pages/Dashboard/Home/Dashboard.jsx
+++ src/Pages/Dashboard/Home/Dashboard.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import '../Dashboard.css'
+import React from "react";
+import "../Dashboard.css";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
+  const data = useSelector((state) => state.portfolioData.data);
+  const status = useSelector((state) => state.portfolioData.status);
 
   return (
     <>
-    <h1>Dashboard Page</h1>
+      <div className="dashboardContainer">
+        <h1>Dashboard Page</h1>
+        {status === "idle" ? (
+          <>
+            <div>No of technologies Added: {data.technologiesData.length}</div>
+            <div>No of projects Added: {data.projectData.length}</div>
+            <div>No of skills Added: {data.skillData.length}</div>
+            <div>No of technologies Added: {data.timelineData.length}</div>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
